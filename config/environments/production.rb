2a0567bbd7952bcd93adc2f2f1b08eaaceb29f8e
Manabu Ito <m.ito09017@gmail.com>
2020-10-22 06:36:20 +0000

    mail test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,10 +64,9 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'mysterious-wildwood-11424.herokuapp.com'
+  host = 'warm-refuge-75320.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
